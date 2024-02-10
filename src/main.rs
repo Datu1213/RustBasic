c97f8c6a2607ad89643bea4f32b1e1f6222d2db9
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-10 14:20:28 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+fn greet_world() {
+    let southern_germany: &str = "Grüß Gott!";
+    let chinese: &str = "世界，你好";
+    let english: &str = "World, hello";
+    let regions: [&str; 3] = [southern_germany, chinese, english];
+    for region in regions {
+        println!("{}", &region);
+    }
+    println!("{}", &chinese);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    // greet_world();
+
+    // f --> float; i --> int; u --> unsigned int; suffix number --> number of bits;
+    let a = 3.1415926; // Without type indication.
+    let b: f32 = 4.1415926; // With    type indication.
+    let c = 3i32;      // With    type indication as a suffix of value.
+    let d = 3_u32;     // With    type indication as a suffix of value, separated by "_".
+    // "=" means "binding" but "assignment"
+    
 }
