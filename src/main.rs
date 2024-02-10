2e16ac66669ff97ddaed8796bbfd1d30d81b6b74
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-10 14:32:55 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,25 @@ fn main() {
 
     // greet_world();
 
-    // f --> float; i --> int; u --> unsigned int; suffix number --> number of bits;
+    // Basic value and type
+    //              f --> float
+    //              i --> int
+    //              u --> unsigned int
+    //  suffix number --> number of bits
+    // Example: f64 --> float with 64bits
     let a = 3.1415926; // Without type indication.
     let b: f32 = 4.1415926; // With    type indication.
     let c = 3i32;      // With    type indication as a suffix of value.
     let d = 3_u32;     // With    type indication as a suffix of value, separated by "_".
     // "=" means "binding" but "assignment"
+
+    // "let" means a immutable variable, it can not change after assigned to a value.
+    // "d = 2;"  will cause an error: "cannot assign twice to immutable variable".
+
+    // Use "mut" to make it mutable
+    let mut e: u8 = 0;
+    e = 1;
+    
+
     
 }
