56df809d3ffe97f2aa62900e91d7de8a50245d26
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-11 23:58:16 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,24 @@ fn greet_world() {
     println!("{}", &chinese);
 }
 
+fn float_check() {
+    let abc: (f32, f32, f32) = (0.1, 0.2, 0.3);
+    let xyz: (f64, f64, f64) = (0.1, 0.2, 0.3);
+
+    println!("abc (f32)");
+    println!("   0.1 + 0.2: {:x}", (abc.0 + abc.1).to_bits());
+    println!("         0.3: {:x}", (abc.2).to_bits());
+    println!();
+
+    println!("xyz (f64)");
+    println!("   0.1 + 0.2: {:x}", (xyz.0 + xyz.1).to_bits());
+    println!("         0.3: {:x}", (xyz.2).to_bits());
+    println!();
+
+    assert!(abc.0 + abc.1 == abc.2);
+    assert!(xyz.0 + xyz.1 == xyz.2);
+}
+
 fn main() {
     // println!("Hello, world!");
 
@@ -77,4 +95,6 @@ fn main() {
     println!("{}", b);  // 19
 
     assert!(0.1 + 0.2 == 0.3);// ERROR!
+
+    float_check();
 }
