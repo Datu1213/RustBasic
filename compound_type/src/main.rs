7f881a4cc7e7bb1aa5532385117adbddaaeb2a18
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-13 22:31:33 +0800

    Update main.rs

--- compound_type/src/main.rs
+++ compound_type/src/main.rs
@@ -2,16 +2,16 @@
 
 fn string_slice() {
     let mut str1: String = String::from("Hello, world");
-    let str_slice1 = &str1[1..2]; // slice from index 1 to 2.
-    let str_slice2 = &str1[ ..2]; // slice from start   to 2.
-    let str_slice3 = &str1[1.. ]; // slice from index 1 to end.
-    let str_slice3 = &str1[ .. ]; // slice from start   to end.
+    let str_slice1: &str = &str1[1..2]; // slice from index 1 to 2.
+    let str_slice2: &str = &str1[ ..2]; // slice from start   to 2.
+    let str_slice3: &str = &str1[1.. ]; // slice from index 1 to end.
+    let str_slice3: &str = &str1[ .. ]; // slice from start   to end.
     // All slices above are immutable reference!!!
 }
 
 fn int_slice() {
-    let array_int = [1, 2, 3, 4, 5]; // Do not use range;
-    let array_slice = &array_int[1..];
+    let array_int: [i32; 5] = [1, 2, 3, 4, 5]; // Do not use range;
+    let array_slice: &[i32] = &array_int[1..];
     assert_eq!(array_slice, &[2, 3, 4, 5]);
 }
 
@@ -27,6 +27,15 @@ fn ref_scope_and_var_scope() {
     // Dirty Data Warning. 
 }
 
+fn chinese_clice() {
+    let str: String = String::from("中国话");
+    let str_ref = &str[0..1];
+    println!("{}", str_ref); //Error, chinese character occupies 3 Bytes.
+    // byte index 1 is not a char boundary; it is inside '中' (bytes 0..3) of `中国话`
+}
+
 fn main() {
-    int_slice();
+    // int_slice();
+
+    // chinese_clice();
 }
