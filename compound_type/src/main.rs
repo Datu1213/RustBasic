7bd462f6e001392f99def555b5be2cefbe38ac4f
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-14 12:24:46 +0800

    Update main.rs

--- compound_type/src/main.rs
+++ compound_type/src/main.rs
@@ -27,11 +27,12 @@ fn ref_scope_and_var_scope() {
     // Dirty Data Warning. 
 }
 
-fn chinese_clice() {
+fn chinese_clice() { // No string index in Rust.
     let str: String = String::from("中国话");
     let str_ref = &str[0..1];
     println!("{}", str_ref); //Error, chinese character occupies 3 Bytes.
     // byte index 1 is not a char boundary; it is inside '中' (bytes 0..3) of `中国话`
+    // println!("{}", &str[1]); // Error.
 }
 
 fn push_string() {
@@ -57,7 +58,7 @@ fn insert_string() {
 
 fn replace_string() {
     let mut s = String::from("Hello pineapple!");
-    s.replace("pineapple", "apple"); // Change itself, use "&mut self".
+    s.replace("pineapple", "apple"); // Replace all matches of a pattern, change itself, use "&self".
     s = s.replacen("p", "P", 3); // Not intrinsic, it has a return value.
 
     s.replace_range(0..=4, "HELLO_suffix"); // They don't have to have a same length.
@@ -66,6 +67,70 @@ fn replace_string() {
 
     println!("{}", s); // HELLO_suffix PineaPPle!
 }
+
+fn pop_string() {
+    let mut string_pop = String::from("pop 21");
+    let p1 = string_pop.pop(); // Delete last "char", and return it or none.
+    let p2 = string_pop.pop();
+    if p1 != None {
+        println!("{:?}", p1);
+    }
+    if p2!= None {
+        println!("{:?}", p2);
+    }
+    dbg!(p1);
+    dbg!(p2);
+    dbg!(string_pop);
+}
+
+fn remove_string() {
+    let mut string_remove = String::from("测试remove方法");
+    println!(
+        "string_remove occupies {} Bytes",
+        std::mem::size_of_val(string_remove.as_str())
+    );
+    // Remove first chinese character.
+    let c = string_remove.remove(0); // Delete a "char" with specific index, and return it.
+    // Error, wrong chinese character start index.
+    // string_remove.remove(1);
+
+    // Remove second chinese character.
+    // string_remove.remove(3);
+    
+    // dbg!(string_remove);
+
+    // let d = string_remove.remove(234); // No index check or "Option", it's unsafe, be careful.
+
+    // And clean string.
+    string_remove.clear();
+}
+
+fn concatenate_string_with_add() {
+    let string_append = String::from("hello "); 
+    let string_rust = String::from("rust"); 
+    
+    // Added string should be a Slice or &str.
+    // &string_rust will automatically deconstruct into &str
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    let result = string_append + &string_rust; // It's not clone!!!!!  Value moved here.    /////
+    /////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Reffer to it as "let result = string_append.add(&string_rust);
+    // Ownership of string_append move to add(), then add() release it.
+
+    // Shadow occurs here
+    let mut result = result + "!"; // `result` in `result + "!"` is immutable.
+    result += "!!!"; 
+
+    println!("concatenate_string + -> {}", result);
+}
+
+fn concatenate_string_with_format() {
+    let hello = String::from("Hello");
+    let world = "world!";
+    let new_str = format!("{}, {}", hello, world);
+
+    println!("{}", new_str);
+}
 fn main() {
     // int_slice();
 
@@ -73,7 +138,15 @@ fn main() {
 
     // push_string();
 
-    insert_string();
+    // insert_string();
 
-    replace_string();
+    // replace_string();
+
+    // pop_string();
+
+    // remove_string();
+
+    //concatenate_string_with_add();
+
+    concatenate_string_with_format();
 }
