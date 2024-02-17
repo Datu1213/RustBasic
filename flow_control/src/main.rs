fe9b58715690bc68d74c5a8cb30a0bec0c0249b1
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-17 14:41:38 +0800

    Update main.rs

--- flow_control/src/main.rs
+++ flow_control/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 fn for_loop() {
     for i in 1..=5 {
         println!("{}", i);
@@ -5,6 +6,32 @@ fn for_loop() {
     
 }
 
+fn for_move() {
+    // for i in collection : Ownership will move.
+    let strs: [String; 8] = std::array::from_fn(|_i| format!("It is {}", _i));
+    for i in strs {
+        println!("{}", i);
+    }
+    // let str: &String = &strs[0]; borrow of moved value
+}
+
+fn for_borrow() {
+    // for i in &collection : Immutable reference.
+    let strs: [String; 8] = std::array::from_fn(|_i| format!("It is {}", _i));
+    for i in &strs {
+        println!("{}", i);
+    }
+    let str: &String = &strs[0]; 
+}
+
+fn for_mut_borrow() {
+    // for i in &mut collection : Mutable reference.
+    let mut strs: [String; 8] = std::array::from_fn(|_i| format!("It is {}", _i));
+    for i in &mut strs {
+        println!("{}", i);
+    }
+    let str: &String = &strs[0]; 
+}
 fn main() {
     println!("Hello, world!");
 }
