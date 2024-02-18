48bf6132073d967d911dc059c84552ee0d539cbb
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-18 23:54:41 +0800

    Update main.rs

--- mode_match/src/main.rs
+++ mode_match/src/main.rs
@@ -1,4 +1,42 @@
 #![allow(unused)]
+enum Direction {
+    East,
+    West,
+    North,
+    South,
+}
+///
+// match target {
+//     模式1 => 表达式1,
+//     模式2 => {
+//         语句1;
+//         语句2;
+//         表达式2
+//     },
+//     _ => 表达式3
+// }
+///
+fn match_test() {
+    let dire = Direction::South;
+    match dire {
+        Direction::East => println!("East"),
+        Direction::North | Direction::South => {
+            println!("South or North");
+        },
+        _ => println!("West"),
+    };
+}
+
+fn match_and_assign() {
+    let ip1 = Direction::West;
+    let ip_str = match ip1 {
+        Direction::West => "West",
+        _ => "::1",
+    };
+
+    println!("{}", ip_str);
+}
 fn main() {
     println!("Hello, world!");
 }
+
