f0b4f21d091069111650bffd798f98c1f5d10864
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-19 22:28:09 +0800

    Update main.rs

--- mode_match/src/main.rs
+++ mode_match/src/main.rs
@@ -56,9 +56,37 @@ fn only_one_match() {
     }
 }
 
+fn shadowing_in_match() {
+    let age = Some(30);
+    println!("Before: {:?}",age);
+    match age {
+        // Some(age) =>  println!("Matched: {}", age), // Shadow
+        Some(x) =>  println!("Matched: {}", x), // Better way.
+        _ => ()
+    }
+   println!("After: {:?}",age);
+}
+
+fn deconstruct_option() {
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+    
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?} {:?}", six, none);
+}
 fn main() {
     // macros_matches();
 
-    only_one_match();
+    // only_one_match();
+
+    // shadowing_in_match();
+
+    deconstruct_option();
 }
 
