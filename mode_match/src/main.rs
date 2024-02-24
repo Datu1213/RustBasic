dc26bb1fbf0f5b213d466a1a2dca8d02603bba93
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-24 23:37:56 +0800

    Update main.rs

--- mode_match/src/main.rs
+++ mode_match/src/main.rs
@@ -294,6 +294,37 @@ fn ignore() {
     }
 
     println!("{:?}", s);
+
+    // Use `..` to ignore the rest of values.
+    struct Point {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+    
+    let origin = Point { x: 0, y: 0, z: 0 };
+    
+    match origin {
+        Point { x, .. } => println!("x is {}", x),
+    }
+
+    // Ignore interval values.
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {}, {}", first, last);
+        },
+    }
+
+    // `..` has to be explicit.
+    // let numbers = (2, 4, 8, 16, 32);
+
+    // match numbers {
+    //     (.., second, ..) => {  // Error!!!!!!!
+    //         println!("Some numbers: {}", second)
+    //     },
+    // }
 }
 fn main() {
     // macros_matches();
