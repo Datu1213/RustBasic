26e88b51244f9a7a94ceae57055c0ec6c88057b3
Zhenkui Wang <zhenkuiwang1213@gmail.com>
2024-02-25 22:19:55 +0800

    Update main.rs

--- method/src/main.rs
+++ method/src/main.rs
@@ -1,20 +1,37 @@
 #![allow(unused)]
 
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
+pub struct Rectangle { // Struct
+    width: u32, 
     height: u32,
 }
 
-impl Rectangle {
-    
+// Use `impl`
+impl Rectangle { // Methods of Struct
+    pub fn new(width: u32, height: u32) -> Self { // `Self` means the Strut we use, it's Rectangle now.
+        return Rectangle {
+            width,
+            height
+        };
+    }
 
-    fn area(&self) -> u32 {
-        self.width * self.height
+    pub fn area(&self) -> u32 { // `&self` is abbreviation of `self: &Self`
+        
+        return self.width * self.height;
+    }
+
+    pub fn width(&self) -> u32{ // Members are private for files outside.
+        return self.width; // Methods' names can be as same as members'.
+    } // It always used to make getter function.
+
+    pub fn height(&self) -> u32{
+        return self.height;
     }
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    let rec = Rectangle::new(1, 2);
+    
+    println!("{:3}", rec.width);
 }
